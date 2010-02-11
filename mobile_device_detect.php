0f1a410949430bab6ecbe5de7223c5c75c3bf9ca
Steve Klabnik <steve@steveklabnik.com>
2010-02-10 23:09:15 -0500

    adding GPL notices to all files, as well as the full license text in COPYING

--- mobile_device_detect.php
+++ mobile_device_detect.php
@@ -1,4 +1,24 @@
+
 <?php
+
+/*
+ * This file is part of Crows.
+ *
+ * Crows is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Crows is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Crows.  If not, see <http://www.gnu.org/licenses/>.
+ *  */
+
+
 $page_url= 'http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 /*
 
--- private/index.php
+++ private/index.php
@@ -3,8 +3,26 @@
  * Crows - Crowd Syndication 1.0
  * Copyright 2009
  * contact@crowsne.st
- * http://www.crowsne.st/license
  */
+
+
+/*
+ * This file is part of Crows.
+ *
+ * Crows is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Crows is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Crows.  If not, see <http://www.gnu.org/licenses/>.
+ *  */
+
 
 
 include('../config.php');
